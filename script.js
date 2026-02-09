6e3677df44bf22167a603c76a8298dd313b49647
Muhammad Naveed <naveed5333425@gmail.com>
2026-02-09 17:37:07 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+const city = document.getElementById("cinput");
+const submit = document.getElementById("output");
+const loc = document.getElementById("loc");
+const temp = document.getElementById("temperature");
+
+async function getWeather() {
+    const cityname = city.value.trim();
+
+    if (cityname === "") {
+        alert("Please enter a city name");
+        return;
+    }
+
+    const apikey = "7bf69d7cc25b4723857195640260702";
+    const url = `https://api.weatherapi.com/v1/current.json?key=${apikey}&q=${cityname}&aqi=no`;
+
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+
+        loc.textContent = "Location : " + data.location.name;
+        temp.textContent = "Temperature : " + data.current.temp_c + " °C";
+    } catch (error) {
+        console.error("Error fetching weather data:", error);
+        loc.textContent = "Error fetching weather data";
+        temp.textContent = "";
+    }
+}
+submit.addEventListener("click", getWeather);
